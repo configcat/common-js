ca2fa79f6f0ab3e92afb05498c1e996029f8e934
ConfigCat <developer@configcat.com>
2018-11-13 19:14:58 +0100

    [skipci] Formatting

--- src/ConfigCatLogger.ts
+++ src/ConfigCatLogger.ts
@@ -3,9 +3,9 @@ import { IConfigCatLogger } from ".";
 export class ConfigCatConsoleLogger implements IConfigCatLogger {
     log(message: string): void {
         console.log('ConfigCat log: ' + message);
-    }
+    };
     
     error(message: string): void {
         console.error('ConfigCat log: ' + message);
-    }
+    };
 }
