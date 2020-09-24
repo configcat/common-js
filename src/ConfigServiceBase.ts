11795a1924aa24ae269c75937a61b3681a5fe694
Lajos <lajos88.szoke@gmail.com>
2020-09-24 15:26:38 +0200

    Better cache handling

--- src/ConfigServiceBase.ts
+++ src/ConfigServiceBase.ts
@@ -41,64 +41,62 @@ export abstract class ConfigServiceBase {
     private fetchLogic(options: OptionsBase, lastProjectConfig: ProjectConfig, retries: number, callback: (newProjectConfig: ProjectConfig) => void): void {
         this.configFetcher.fetchLogic(this.baseConfig, lastProjectConfig, (newConfig) => {
 
-            if (newConfig && newConfig.ConfigJSON) {
-
-                const preferences = newConfig.ConfigJSON[ConfigFile.Preferences];
-                if (!preferences) {
-
-                    callback(newConfig);
-                    return;
-                }
-
-                const baseUrl = preferences[Preferences.BaseUrl];
-
-                // If the base_url is the same as the last called one, just return the response.
-                if (!baseUrl || baseUrl == options.baseUrl) {
-
-                    callback(newConfig);
-                    return;
-                }
-
-                const redirect = preferences[Preferences.Redirect];
-
-                // If the base_url is overridden, and the redirect parameter is not 2 (force),
-                // the SDK should not redirect the calls and it just have to return the response.
-                if (options.baseUrlOverriden && redirect !== 2) {
-                    callback(newConfig);
-                    return;
-                }
-
-                options.baseUrl = baseUrl;
-
-                if (redirect === 0) {
-
-                    callback(newConfig);
-                    return;
-                }
-
-                if (redirect === 1) {
-
-                    options.logger.warn("Please check the data_governance parameter in the ConfigCatClient initialization. " +
-                        "It should match the settings provided in " +
-                        "https://app.configcat.com/organization/data-governance." +
-                        "If you are not allowed to view this page, ask your Organization's Admins " +
-                        "for the correct setting.");
-                }
-
-                if (retries >= 2) {
-                    options.logger.error("Redirect loop during config.json fetch. Please contact support@configcat.com.");
-                    callback(newConfig);
-                    return;
-                }
-
-                this.fetchLogic(options, lastProjectConfig, ++retries, callback);
+            if (!newConfig || !newConfig.ConfigJSON) {
+                callback(null);
                 return;
             }
-            else {
 
-                callback(lastProjectConfig);
+            const preferences = newConfig.ConfigJSON[ConfigFile.Preferences];
+            if (!preferences) {
+
+                callback(newConfig);
                 return;
             }
-        });
+
+            const baseUrl = preferences[Preferences.BaseUrl];
+
+            // If the base_url is the same as the last called one, just return the response.
+            if (!baseUrl || baseUrl == options.baseUrl) {
+
+                callback(newConfig);
+                return;
+            }
+
+            const redirect = preferences[Preferences.Redirect];
+
+            // If the base_url is overridden, and the redirect parameter is not 2 (force),
+            // the SDK should not redirect the calls and it just have to return the response.
+            if (options.baseUrlOverriden && redirect !== 2) {
+                callback(newConfig);
+                return;
+            }
+
+            options.baseUrl = baseUrl;
+
+            if (redirect === 0) {
+
+                callback(newConfig);
+                return;
+            }
+
+            if (redirect === 1) {
+
+                options.logger.warn("Please check the data_governance parameter in the ConfigCatClient initialization. " +
+                    "It should match the settings provided in " +
+                    "https://app.configcat.com/organization/data-governance." +
+                    "If you are not allowed to view this page, ask your Organization's Admins " +
+                    "for the correct setting.");
+            }
+
+            if (retries >= 2) {
+                options.logger.error("Redirect loop during config.json fetch. Please contact support@configcat.com.");
+                callback(newConfig);
+                return;
+            }
+
+            this.fetchLogic(options, lastProjectConfig, ++retries, callback);
+            return;
+        }
+        );
     }
 }
