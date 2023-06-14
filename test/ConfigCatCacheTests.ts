89522893b8f0721ad422137310656a8b2a0094da
Adam Simon <adamosimoni@gmail.com>
2023-06-14 18:46:31 +0200

    Add tests for ensuring that shared caching works correctly across SDKs

--- test/ConfigCatCacheTests.ts
+++ test/ConfigCatCacheTests.ts
@@ -1,6 +1,6 @@
 import { assert } from "chai";
 import "mocha";
-import { } from "../src";
+import { ManualPollOptions } from "../lib/ConfigCatClientOptions";
 import { ExternalConfigCache, IConfigCache, IConfigCatCache, InMemoryConfigCache, } from "../src/ConfigCatCache";
 import { LogLevel, LoggerWrapper } from "../src/ConfigCatLogger";
 import { Config, ProjectConfig } from "../src/ProjectConfig";
@@ -50,48 +50,68 @@ describe("ConfigCatCache", () => {
       }
     });
   }
-});
 
-it(`${ExternalConfigCache.prototype.constructor.name} should handle when external cache fails`, async () => {
-  const cacheKey = "";
+  it(`${ExternalConfigCache.prototype.constructor.name} should handle when external cache fails`, async () => {
+    const cacheKey = "";
 
-  const logger = new FakeLogger(LogLevel.Warn);
+    const logger = new FakeLogger(LogLevel.Warn);
 
-  const externalCache = new FaultyFakeExternalCache();
-  const configCache = new ExternalConfigCache(externalCache, new LoggerWrapper(logger));
+    const externalCache = new FaultyFakeExternalCache();
+    const configCache = new ExternalConfigCache(externalCache, new LoggerWrapper(logger));
 
-  // 1. Initial read should return the empty config.
-  let cachedConfig = await configCache.get(cacheKey);
+    // 1. Initial read should return the empty config.
+    let cachedConfig = await configCache.get(cacheKey);
 
-  assert.equal(ProjectConfig.empty, cachedConfig);
+    assert.equal(ProjectConfig.empty, cachedConfig);
 
-  assert.equal(1, logger.messages.filter(([level, eventId, _, err]) =>
-    level === LogLevel.Error && eventId === 2200 && (err as Error).message === "Operation failed :(").length);
+    assert.equal(1, logger.messages.filter(([level, eventId, _, err]) =>
+      level === LogLevel.Error && eventId === 2200 && (err as Error).message === "Operation failed :(").length);
 
-  // 2. Set should overwrite the local cache and log the error.
+    // 2. Set should overwrite the local cache and log the error.
 
-  logger.messages.length = 0;
+    logger.messages.length = 0;
 
-  const configJson = "{\"p\": {\"u\": \"http://example.com\", \"r\": 0}}";
-  const config = new ProjectConfig(configJson, new Config(configJson), ProjectConfig.generateTimestamp(), "\"ETAG\"");
+    const configJson = "{\"p\": {\"u\": \"http://example.com\", \"r\": 0}}";
+    const config = new ProjectConfig(configJson, new Config(configJson), ProjectConfig.generateTimestamp(), "\"ETAG\"");
 
-  await configCache.set(cacheKey, config);
+    await configCache.set(cacheKey, config);
 
-  assert.equal(config, configCache["cachedConfig"]);
+    assert.equal(config, configCache["cachedConfig"]);
 
-  assert.equal(1, logger.messages.filter(([level, eventId, _, err]) =>
-    level === LogLevel.Error && eventId === 2201 && (err as Error).message === "Operation failed :(").length);
+    assert.equal(1, logger.messages.filter(([level, eventId, _, err]) =>
+      level === LogLevel.Error && eventId === 2201 && (err as Error).message === "Operation failed :(").length);
 
-  // 3. Get should log the error and return the local cache which was set previously.
+    // 3. Get should log the error and return the local cache which was set previously.
 
-  logger.messages.length = 0;
+    logger.messages.length = 0;
 
-  cachedConfig = await configCache.get(cacheKey);
+    cachedConfig = await configCache.get(cacheKey);
 
-  assert.equal(config, cachedConfig);
+    assert.equal(config, cachedConfig);
 
-  assert.equal(1, logger.messages.filter(([level, eventId, _, err]) =>
-    level === LogLevel.Error && eventId === 2200 && (err as Error).message === "Operation failed :(").length);
+    assert.equal(1, logger.messages.filter(([level, eventId, _, err]) =>
+      level === LogLevel.Error && eventId === 2200 && (err as Error).message === "Operation failed :(").length);
+  });
+
+  for (const [sdkKey, expectedCacheKey] of [
+    ["test1", "147c5b4c2b2d7c77e1605b1a4309f0ea6684a0c6"],
+    ["test2", "c09513b1756de9e4bc48815ec7a142b2441ed4d5"],
+  ]) {
+    it(`Cache key generation should be platform independent - ${sdkKey}`, () => {
+      const options = new ManualPollOptions(sdkKey, "common", "1.0.0");
+      assert.strictEqual(options.getCacheKey(), expectedCacheKey);
+    });
+  }
+
+  const payloadTestConfigJson = "{\"p\":{\"u\":\"https://cdn-global.configcat.com\",\"r\":0},\"f\":{\"testKey\":{\"v\":\"testValue\",\"t\":1,\"p\":[],\"r\":[]}}}";
+  for (const [configJson, timestamp, httpETag, expectedPayload] of [
+    [payloadTestConfigJson, "2023-06-14T15:27:15.8440000Z", "test-etag", "1686756435844\ntest-etag\n" + payloadTestConfigJson],
+  ]) {
+    it(`Cache payload serialization should be platform independent - ${httpETag}`, () => {
+      const pc = new ProjectConfig(configJson, JSON.parse(configJson), Date.parse(timestamp), httpETag);
+      assert.strictEqual(ProjectConfig.serialize(pc), expectedPayload);
+    });
+  }
 });
 
 class FakeExternalCache implements IConfigCatCache {
