72dac0f99da0e42103ab64379983f726a892e3ca
Adam Simon <adamosimoni@gmail.com>
2023-11-23 14:20:27 +0100

    Improve word choice in intellisense docs

--- src/User.ts
+++ src/User.ts
@@ -19,36 +19,36 @@ export class User {
     /**
      * Custom attributes of the user for advanced targeting rule definitions (e.g. user role, subscription type, etc.)
      * @remarks
-     * The set of allowed attribute values depends on the comparison type of the condition which references the User Object attribute.
-     * `string` values are supported by all comparison types (in some cases they need to be provided in a specific format though).
-     * Some of the comparison types work with other types of values, as described below.
+     * The set of allowed attribute values depends on the comparator of the condition which references the User Object attribute.
+     * `string` values are supported by all comparators (in some cases they need to be provided in a specific format though).
+     * Some of the comparators work with other types of values, as described below.
      *
-     * Text-based comparisons (EQUALS, IS ONE OF, etc.)<br/>
+     * Text-based comparators (EQUALS, IS ONE OF, etc.)<br/>
      * * accept `string` values,<br/>
      * * all other values are automatically converted to string (a warning will be logged but evaluation will continue as normal).
      *
-     * SemVer-based comparisons (IS ONE OF, &lt;, &gt;=, etc.)<br/>
+     * SemVer-based comparators (IS ONE OF, &lt;, &gt;=, etc.)<br/>
      * * accept `string` values containing a properly formatted, valid semver value,<br/>
      * * all other values are considered invalid (a warning will be logged and the currently evaluated targeting rule will be skipped).
      *
-     * Number-based comparisons (=, &lt;, &gt;=, etc.)<br/>
+     * Number-based comparators (=, &lt;, &gt;=, etc.)<br/>
      * * accept `number` values,<br/>
      * * accept `string` values containing a properly formatted, valid `number` value,<br/>
      * * all other values are considered invalid (a warning will be logged and the currently evaluated targeting rule will be skipped).
      *
-     * Date time-based comparisons (BEFORE / AFTER)<br/>
+     * Date time-based comparators (BEFORE / AFTER)<br/>
      * * accept `Date` values, which are automatically converted to a second-based Unix timestamp,<br/>
      * * accept `number` values representing a second-based Unix timestamp,<br/>
      * * accept `string` values containing a properly formatted, valid `number` value,<br/>
      * * all other values are considered invalid (a warning will be logged and the currently evaluated targeting rule will be skipped).
      *
-     * String array-based comparisons (ARRAY CONTAINS ANY OF / ARRAY NOT CONTAINS ANY OF)<br/>
+     * String array-based comparators (ARRAY CONTAINS ANY OF / ARRAY NOT CONTAINS ANY OF)<br/>
      * * accept arrays of `string`,<br/>
      * * accept `string` values containing a valid JSON string which can be deserialized to an array of `string`,<br/>
      * * all other values are considered invalid (a warning will be logged and the currently evaluated targeting rule will be skipped).
      *
      * In case a non-string attribute value needs to be converted to `string` during evaluation, it will always be done using the same format
-     * which is accepted by the comparisons.
+     * which is accepted by the comparators.
      **/
     public custom: { [key: string]: UserAttributeValue } = {}
   ) {
