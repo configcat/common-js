3dda45128a63d8422fc8ed88923e44a48a0b3019
Adam Simon <adamosimoni@gmail.com>
2024-01-29 15:16:09 +0100

    Use valid SDK keys in cache key generation tests

--- test/ConfigCatCacheTests.ts
+++ test/ConfigCatCacheTests.ts
@@ -94,8 +94,8 @@ describe("ConfigCatCache", () => {
   });
 
   for (const [sdkKey, expectedCacheKey] of [
-    ["test1", "7f845c43ecc95e202b91e271435935e6d1391e5d"],
-    ["test2", "a78b7e323ef543a272c74540387566a22415148a"],
+    ["configcat-sdk-1/TEST_KEY-0123456789012/1234567890123456789012", "f83ba5d45bceb4bb704410f51b704fb6dfa19942"],
+    ["configcat-sdk-1/TEST_KEY2-123456789012/1234567890123456789012", "da7bfd8662209c8ed3f9db96daed4f8d91ba5876"],
   ]) {
     it(`Cache key generation should be platform independent - ${sdkKey}`, () => {
       const options = new ManualPollOptions(sdkKey, "common", "1.0.0");
